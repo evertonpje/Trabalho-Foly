b36daaaecec7330e9a9d09fec5dd3012b076e5ee
Lucas Miranda <lucasmsoares72@gmail.com>
2024-11-22 12:37:46 -0300

    Starting JavaFX Boilerplate
    First commit

---
+++ SteamCollection/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module dev.work.lucaseeverton.steamcollection {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens dev.work.lucaseeverton.steamcollection to javafx.fxml;
+    exports dev.work.lucaseeverton.steamcollection;
+}
